c4de6a6b75c018da34112077ec9c44dd55fe6470
Massimiliano <m.perletti2@campus.unimib.it>
2019-06-08 13:02:18 +0200

    update viz survey

--- Visualizations/survey/corr_questionario_1.R
+++ Visualizations/survey/corr_questionario_1.R
@@ -1,14 +1,19 @@
-df <- read.csv('Questionario psicometrico (VIZ_1).csv')
+#df <- read.csv('Questionario psicometrico (VIZ_1).csv')
+df <- read.table('https://raw.githubusercontent.com/DBertazioli/ARmeetup/master/Visualizations/survey/Questionario%20psicometrico%20(VIZ_1).txt', header = TRUE, sep = ",")
 df_corr <- df[,-1]
+df_corr <- df_corr[, -7]
 
 mcor <- cor(df_corr, method = 'kendall')
 
 install.packages('corrplot')
-png("test.png", width = 8, height = 8, units = 'in', res = 1000)
+
+#change resolution
+png("corr_plot_viz1.png", width = 10, height = 10, units = 'in', res = 800)
+
 library(corrplot)
 corrplot(mcor, method = 'circle',
          type = 'upper', order = 'hclust', tl.col = 'black',
-         tl.srt = 45, tl.cex = 0.6, addCoef.col  = 'black', number.cex = 0.55,
-         p.mat = res1$p, insig = 'p-value')
+         tl.srt = 45, tl.cex = 0.7, addCoef.col  = 'black', number.cex = 0.85)
+
 dev.off()
 
