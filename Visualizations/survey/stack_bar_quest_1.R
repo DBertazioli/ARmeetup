debba6c6ae0091cc8ddbf22be68cd6fd29ee5e04
maxi93 <m.perletti2@campus.unimib.it>
2019-06-09 21:55:52 +0200

    fix survey stacked...

--- Visualizations/survey/stack_bar_quest_1.R
+++ Visualizations/survey/stack_bar_quest_1.R
@@ -57,64 +57,66 @@ ggplot(reshaped,
            y = answer,
            x = question)) + 
   geom_bar(stat = "identity",
-           position = "fill") +
+           position = "fill",
+           width = 0.95) +
   geom_errorbar(data = reshaped_error,
                 aes(x = question,
                     ymax = up,
                     ymin = low),
                 width = .15) +
-  geom_hline(yintercept = 0.50, linetype = "dashed", color = "black", size = 1) +
+  geom_hline(yintercept = 0.50, linetype = "dashed", color = "white", size = 0.5) +
   coord_flip() +
-  theme_bw() # versione base, poi puoi arricchirlo come ti piace
-  # theme_classic(base_size = 14, base_family = "Helvetica") +
-  # theme(axis.text.x=element_text(size=9)) +
-  # theme(axis.title.y=element_text(size=12, face="bold", vjust=1)) +
-  # theme(axis.text.y=element_text(angle=45,hjust=1,vjust=1, size=11)) +
-  # theme(legend.position="right")
+  geom_text(aes(label = format(round(reshaped$answer*100, 1))), position = position_stack(vjust = 0.5), size = 3, color = 'white') +
+  theme_classic(base_size = 12, base_family = "Helvetica") + # versione base, poi puoi arricchirlo come ti piace
+  theme(axis.text.x=element_text(size=10)) +
+  theme(axis.title.y=element_text(size=14, face="bold", vjust=1)) +
+  theme(axis.title.x=element_text(size=14, face="bold", vjust=1)) +
+  theme(axis.text.y=element_text(hjust=1,vjust=1, size=10))
+  #theme(legend.position="right")
 
 
 # da qui riprende tuo script ----------------------------------------------
-df_stack <- t(df_stack)
-df_stack
-
-df_stack <- as.data.frame(df_stack)
-df_stack
-
-# df_stack$Group <- row.names(df_stack)
-# rownames(df_stack) = 1:6
-
-negativo <- rowSums(df_stack[1:6,] == 0)
-positivo <- rowSums(df_stack[1:6,] == 1)
-df_fin <- as.data.frame(cbind(negativo, positivo))
-df_fin$Group <- row.names(df_fin)
-rownames(df_fin) = 1:6
-
-library(ggplot2)
-library(plyr)
-library(reshape2)
-library(tibble)
-
-melted <- melt(df_fin, id.vars='Group')
-# means <- ddply(melted, c('variable','Group'), summarise,
-#                mean=mean(value))
-melted
-ggplot(data=melted, aes(x=Group, y=value, fill=variable)) + 
-  geom_bar(stat="identity",
-           width = 0.5) +                           
-  xlab(" ") + ylab("number of response") + 
-  theme_classic(base_size = 14, base_family = "Helvetica") + 
-  theme(axis.text.y=element_text(size=9)) + 
-  theme(axis.title.y=element_text(size=12, face="bold", vjust=1)) + 
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1, size=11)) +
-  theme(legend.position="right")
-
-# Calc SEM  
-means.sem <- ddply(melted, c("variable", "Group"), summarise,
-                   mean=mean(value), sem=sd(value)/sqrt(length(value)))
-means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
-means.sem[means.sem$variable=='Labeled',5:6] <- means.sem[means.sem$variable=='Labeled',3] + means.sem[means.sem$variable=='Unlabeled',5:6]
-
-# Add SEM & change appearance of barplot
-plotSEM <- plot + geom_errorbar(data=means.sem, aes(ymax=upper,  ymin=lower), 
-                                width=0.15)
-
+# df_stack <- t(df_stack)
+# df_stack
+# 
+# df_stack <- as.data.frame(df_stack)
+# df_stack
+# 
+# # df_stack$Group <- row.names(df_stack)
+# # rownames(df_stack) = 1:6
+# 
+# negativo <- rowSums(df_stack[1:6,] == 0)
+# positivo <- rowSums(df_stack[1:6,] == 1)
+# df_fin <- as.data.frame(cbind(negativo, positivo))
+# df_fin$Group <- row.names(df_fin)
+# rownames(df_fin) = 1:6
+# 
+# library(ggplot2)
+# library(plyr)
+# library(reshape2)
+# library(tibble)
+# 
+# melted <- melt(df_fin, id.vars='Group')
+# # means <- ddply(melted, c('variable','Group'), summarise,
+# #                mean=mean(value))
+# melted
+# ggplot(data=melted, aes(x=Group, y=value, fill=variable)) + 
+#   geom_bar(stat="identity",
+#            width = 0.5) +                           
+#   xlab(" ") + ylab("number of response") + 
+#   theme_classic(base_size = 14, base_family = "Helvetica") + 
+#   theme(axis.text.y=element_text(size=9)) + 
+#   theme(axis.title.y=element_text(size=12, face="bold", vjust=1)) + 
+#   theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1, size=11)) +
+#   theme(legend.position="right")
+# 
+# # Calc SEM  
+# means.sem <- ddply(melted, c("variable", "Group"), summarise,
+#                    mean=mean(value), sem=sd(value)/sqrt(length(value)))
+# means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
+# means.sem[means.sem$variable=='Labeled',5:6] <- means.sem[means.sem$variable=='Labeled',3] + means.sem[means.sem$variable=='Unlabeled',5:6]
+# 
+# # Add SEM & change appearance of barplot
+# plotSEM <- plot + geom_errorbar(data=means.sem, aes(ymax=upper,  ymin=lower), 
+#                                 width=0.15)
+# 
