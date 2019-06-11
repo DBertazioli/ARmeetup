79ea75bd657821472fa2977683f9a4f16b8f1ce7
maxi93 <m.perletti2@campus.unimib.it>
2019-06-11 11:55:48 +0200

    fix survey

--- Visualizations/survey/corr_questionario_1.R
+++ Visualizations/survey/corr_questionario_1.R
@@ -1,19 +1,22 @@
+library(corrplot)
+
 #df <- read.csv('Questionario psicometrico (VIZ_1).csv')
 df <- read.table('https://raw.githubusercontent.com/DBertazioli/ARmeetup/master/Visualizations/survey/Questionario%20psicometrico%20(VIZ_1).txt', header = TRUE, sep = ",")
 df_corr <- df[,-1]
 df_corr <- df_corr[, -7]
 
-mcor <- cor(df_corr, method = 'kendall')
+mcor <- cor(df_corr, method = 'pearson')
 
-install.packages('corrplot')
+#install.packages('corrplot')
 
 #change resolution
 png("corr_plot_viz1.png", width = 10, height = 10, units = 'in', res = 800)
 
-library(corrplot)
+
 corrplot(mcor, method = 'circle',
          type = 'upper', order = 'hclust', tl.col = 'black',
-         tl.srt = 45, tl.cex = 0.7, addCoef.col  = 'black', number.cex = 0.85)
+         tl.srt = 45, tl.cex = 0.7, addCoef.col  = 'black', number.cex = 0.85,
+         cl.lim=c(0,1), col=colorRampPalette(c("blue","white","red"))(200))
 
 dev.off()
 
